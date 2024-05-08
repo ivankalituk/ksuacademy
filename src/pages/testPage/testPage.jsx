aa0fc3aacb334c5056ca05ae15bbff84509c1d00
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-05-08 21:07:11 +0200

    TESTED IMG

--- src/pages/testPage/testPage.jsx
+++ src/pages/testPage/testPage.jsx
@@ -1,77 +1,52 @@
 import "./testPage.scss"
-
-import sampleImg from '../../assets/photos/courseSampleAvatar.svg'
+import axios from "axios"
 import { useState } from "react"
-import { useRequest } from "../../hooks/hook"
-import { updateCourse } from "../../api/course"
 
 function  TestPage(){
 
-    const [editMode, setEditMode] = useState(false)
-    const [courseInput, setCourseInput] = useState('')
-    const [newImg, setNewImg] = useState(null)
-
-    // post request WITH FILE
-    const {mutatedFunc: putCourse} = useRequest({fetchFunc: updateCourse})
+    const [photoFile, setPhotoFile] = useState(null)
+    const [selectedImg, setSelectedImg] = useState(null)
 
 
-    // submit data func
-    const handleSubmit = () =>{
-        if (courseInput !== ''){
-
-            const data = new FormData();
-            data.append('file', newImg)
-            data.append('gay', courseInput)
-            data.append('gay2', 2 )
-            putCourse(data)
-            // putCourse({
-            //     course_id: 1,
-            //     course_name: courseInput,
-            //     img: newImg
-            // })
-
-            console.log(courseInput)
-            setCourseInput('')
-            setEditMode(false)
-        } else {
-            alert("EMPTY INPUT")
-        }
-    }
-
-    // geting a photo
-    const handleImg = (event) =>{
+    const handlePhoto = (event) => {
         const file = event.target.files[0]
-        const  reader = new FileReader()
+        setPhotoFile(file)
+        const reader = new FileReader()
 
-        reader.onload = () =>{
-            setNewImg(reader.result)
+        reader.onload= () =>{
+            setSelectedImg(reader.result)
         }
 
         reader.readAsDataURL(file)
     }
 
+    const handleUpload = () =>{
+        if (!photoFile) return; // Добавьте проверку на наличие файла
+
+        const formData = new FormData();
+        formData.append('chapter_id', 2)
+        formData.append('file', photoFile);
+
+        axios.put('http://localhost:1000/course', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        .then(response => {
+            console.log(response.data);
+            // Добавьте здесь логику обработки успешного ответа от сервера
+        })
+        .catch(error => {
+            console.error('Error uploading file:', error);
+            // Добавьте здесь логику обработки ошибки
+        });
+    }
 
     return(
         <div className="testPage">
-            <div className="testPage_container">
-            
-                <div className="testPage_heading">COURSES</div>
-
-                <div className="testPage_course">
-                    <div className="testPage_course_img">
-                        <img src={newImg} alt="sampleImg" />
-                        {editMode && <input type="file" onChange={handleImg}/>}
-                    </div>
-                    
-                    {!editMode && <div className="testPage_course_name">NAME</div>}
-                    {editMode && <input type="text" value={courseInput} onChange={(event)=>{setCourseInput(event.target.value)}}/>}
-                </div>
-
-                <div className="testPage_buttons">
-                    <button onClick={() => {setEditMode(!editMode)}}>EDITMODE</button>
-                    <button onClick={handleSubmit}>SUBMIT</button>
-                </div>
-            </div>        
+            <input type="file" id="fileInput" onChange={handlePhoto}/>
+            <button onClick={handleUpload}>SUBMIT</button>
+            <img src={selectedImg} alt="" />
         </div>
     )
 }
