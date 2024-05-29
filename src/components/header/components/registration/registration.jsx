305c4ebb44b2c7c364f134afca4bdf65c2d56909
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-05-29 21:29:30 +0200

    oauth

--- src/components/header/components/registration/registration.jsx
+++ src/components/header/components/registration/registration.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import './registration.scss'
-import { GoogleLogin } from '@react-oauth/google'
+import { GoogleLogin, googleLogout, useGoogleLogin } from '@react-oauth/google'
 import { jwtDecode } from 'jwt-decode'
 
 
@@ -9,6 +9,22 @@ import logo2 from '../../../../assets/photos/logo2.png'
 import axios from 'axios'
 
 function Registration(){
+
+    const login = useGoogleLogin({
+        onSuccess: (data) =>{
+            console.log(data.access_token)
+            console.log(axios.post('http://localhost:1000/user', {access_tocken: data.access_token}))
+        },
+        onError: error => {
+            console.log(error)
+        }
+    })
+
+    const logout = () => {
+        googleLogout()
+         console.log(axios.post('http://localhost:1000/user', {access_tocken: 'ya29.a0AXooCgupMbU3XWWN9z0zLK6fm1OCbbfe90jWyv9Tce4P6V0lliwdKgiYmPH3URLTWg5OzTyWzTAmVe3rby7h69No9LP3S0uY8zKzsJ4hHRyTlQBtLo2PbCm9cx7mB_YgYkc9t2y7rHEFknbpGG5MkiRzlTgkgUG_iJoaCgYKAVQSARASFQHGX2Mi5Lp77nwNmsPiznq3qwXbzA0170'}))
+    }
+
     return(
         <div className="regstration">
 
@@ -22,9 +38,9 @@ function Registration(){
                     ]}
                 />
             </div> */}
-            <GoogleLogin  onSuccess={credentialResponse => {
-                console.log(jwtDecode(credentialResponse.credential));
-            }}/>
+
+            <button onClick={login}>ВХОД</button>
+            <button onClick={logout}>ВЫХОД</button>
             <Link className="header_registrationGroup_Authorized" to="/profile">
 
                 <div className="registrationGroup_Authorized_container">
